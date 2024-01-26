1c9cd132c3318b36999e6ae93b24bb401e219d56
Rachel Gapasin <gapasinrachel@gmail.com>
2024-01-26 01:51:08 -0500

    Check check_answer function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from art import logo
 from random import randint
 
+# set difficulty to determine number of attempts
 def set_difficulty():
   difficulty = ""
   while difficulty not in ["easy", "hard"]:
@@ -11,6 +12,19 @@ def set_difficulty():
   elif difficulty == "hard":
     return 5
   
+# check user's guess with answer
+def check_answer(guess, answer, attempts):
+  is_correct = False
+  if guess == answer:
+    is_correct = True
+    print(f"{guess} is correct! You win!")
+  elif guess > answer:
+    print(f"Too high, {attempts} attempts remaining.")
+  else:
+    print(f"Too low, {attempts} attempts remaining.")
+  
+  return is_correct
+
 def play_game():
   print(logo)
   print("Welcome to the Number Guessing Game!")
@@ -20,20 +34,17 @@ def play_game():
   print(f"You have {attempts} attempts remaining to guess the number.")
 
   continue_game = True
-
   while continue_game:
     guess = int(input("Make a guess: ")) 
 
     attempts -= 1
 
-    if guess == answer:
+    # if user's guess is correct, end the game
+    is_correct = check_answer(guess, answer, attempts)
+    if is_correct:
       continue_game = False
-      print(f"{guess} is correct! You win!")
-    elif guess > answer:
-      print(f"Too high, {attempts} remaining.")
-    else:
-      print(f"Too low, {attempts} remaining.")
 
+    # if user runs out of attempts, end the game
     if attempts == 0 and continue_game == True:
       continue_game = False
       print(f"You've run out of attempts; the correct answer was {answer}. You lose.")
